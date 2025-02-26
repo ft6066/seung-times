018398f44b1d031332f1c25bc83cf55cb22bbff8
Seung Hwang <2018066@naver.com>
2025-02-27 00:43:46 +0900

    8강까지는 진행?

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const API_KEY = `c80114dd430c49e695b9e043cf00f498`;
-let news = [];
+let newsList = [];
+let searchBtn = document.getElementById("search-button");
+let searchOn = false;
+
+searchBtn.addEventListener("click", inputSwitch);
+
 const getLatestName = async () => {
   const url = new URL(
     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
@@ -7,7 +12,63 @@ const getLatestName = async () => {
   const response = await fetch(url);
   const data = await response.json();
   console.log(response);
-  news = data.articles;
-  console.log("data:", news);
+  newsList = data.articles;
+  console.log("data:", newsList);
+  render();
 };
 getLatestName();
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function render() {
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news">
+    <div class="col-lg-4">
+      <img
+        class="news-img-size"
+        src="${
+          news.urlToImage ||
+          "https://thumbs.dreamstime.com/b/image-not-available-icon-vector-set-white-background-eps-338269360.jpg"
+        }"
+      />
+    </div>
+    <div class="col-lg-8">
+      <h2>${news.title}</h2>
+      <p>${
+        news.description == null || news.description == ""
+          ? "내용없음"
+          : news.description.length > 200
+          ? news.description.slice(0, 200) + "..."
+          : news.description
+      }</p>
+      <div>${news.source.name || "no source"} * ${moment(news.publishedAt)
+        .startOf("hour")
+        .fromNow()}</div>
+    </div>
+  </div>`
+    )
+    .join("");
+  console.log("html: ", newsHTML);
+  document.getElementById("news-board").innerHTML = newsHTML;
+}
+
+function inputSwitch() {
+  if (searchOn === false) {
+    document.getElementById("input-news").style.visibility = "visible";
+    document.getElementById("go-button").style.visibility = "visible";
+    searchOn = true;
+  } else if (searchOn === true) {
+    document.getElementById("input-news").style.visibility = "hidden";
+    document.getElementById("go-button").style.visibility = "hidden";
+    searchOn = false;
+  }
+}
